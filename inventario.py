9dac2f2426576462a823f263d9025467b9d916e6
danielvidal2005 <danielvidal2005@gmail.com>
2025-05-16 00:23:40 -0300

    Criação de tabelas de usuários e inventário

---
+++ inventario.py
@@ -0,0 +1,100 @@
+import sqlite3
+from PIL import Image
+import io
+
+
+def criar_tabela_inventario():
+    with sqlite3.connect('inventario.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS inventario (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                item TEXT NOT NULL,
+                quantidade INTEGER NOT NULL   
+                imagem BLOB     
+            )
+        ''')
+        conn.commit()
+
+
+def ler_imagem(caminho_imagem):
+    with open(caminho_imagem, 'rb') as file:
+        blob = file.read()
+    return blob
+
+
+def inserir_item(item, quantidade, caminho_imagem):
+    imagem_blob = ler_imagem(caminho_imagem)
+
+    with sqlite3.connect('inventario.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO inventario
+            (item, quantidade, imagem)
+            VALUES (?, ?, ?)
+        ''', (item, quantidade, imagem_blob))
+        conn.commit()
+
+
+def visualizar_itens():
+    with sqlite3.connect('inventario.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT id, item, quantidade, imagem FROM inventario')
+        itens = cursor.fetchall()
+
+        for item in itens:
+            id_item, nome, quantidade, imagem_blob = item 
+            print(f'\nID: {id_item}\nItem: {nome}\nQuantidade: {quantidade}')
+            
+            if imagem_blob:
+                try:
+                    imagem = Image.open(io.BytesIO(imagem_blob))
+                    imagem.show(title=f'Item: {nome}')
+                except Exception as e:
+                    print('Erro ao exibir imagem:', e)    
+            else:
+                print('Este item não possui imagem.')
+
+
+def atualizar_item(id_item, novo_item=None, nova_quantidade=None, novo_caminho_imagem=None):
+    campos =[]
+    valores =[]
+
+    if novo_item is not None:
+        campos.append('item = ?')
+        valores.append(novo_item)
+    
+    if nova_quantidade is not None:
+        campos.append('quantidade = ?')
+        valores.append(nova_quantidade)
+
+    if novo_caminho_imagem is not None:
+        nova_imagem_blob = ler_imagem(novo_caminho_imagem)
+        campos.append('imagem = ?')
+        valores.append(nova_imagem_blob)
+
+    if not campos:
+        print('Nada a atualizar.')
+        return
+    
+    valores.append(id_item)
+
+    query = f'''
+        UPDATE inventario
+        SET {', '.join(campos)}
+        WHERE id = ?
+    '''
+
+    with sqlite3.connect('inventario.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute(query, valores)
+        conn.commit()
+
+
+def excluir_item(id_item):
+    with sqlite3.connect('inventario.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM inventario WHERE id = ?', (id_item,))
+        conn.commit()
+        print(f'Item com id {id_item} foi excluído com sucesso.')
+
---
+++ usuarios.py
@@ -0,0 +1,56 @@
+import sqlite3
+import bcrypt
+
+
+def criar_tabela_usuario():
+    with sqlite3.connect("usuarios.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTIS usuarios (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome_completo TEXT NOT NULL,
+                login TEXT UNIQUE NOT NULL,
+                senha_hash TEXT NOT NULL,
+                nivel_acesso TEXT NOT NULL    
+            )
+        ''')
+        conn.commit()
+
+
+def cadastrar_usuario(nome_completo: str, login: str, senha: str, nivel_acesso: str) -> bool:
+    try:
+        senha_hash = bcrypt.hashpw(senha.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+        with sqlite3.connect('usuarios.db') as conn:
+            cursor = conn.cursor()
+            cursor.execute('''
+                INSERT INTO usuarios
+                (nome_completo, login, senha_hash, nivel_acesso)
+                VALUES (?, ?, ?, ?)
+            ''', (nome_completo, login, senha_hash, nivel_acesso))
+            conn.commit()
+            return True
+    except sqlite3.IntegrityError:
+        return False
+    except Exception as e:
+        print(F"Erro ao cadastrar usuário: {str(e)}")
+        return False
+    
+
+def validar_login(login:str, senha:str) -> bool:
+    '''Verifica as credenciais do usuário'''
+    try:
+        with sqlite3.connect('usuarios.db') as conn:
+            cursor = conn.cursor()
+            cursor.execute('SELECT senha_hash FROM usuarios WHERE login = ?', (login,))
+            resultado = cursor.fetchone()
+
+        if resultado:
+            senha_hash = resultado[0].encode('utf-8')
+            return bcrypt.checkpw(senha.encode('utf-8'), senha_hash)
+        return False
+    
+    except Exception as e:
+        print(f'Erro ao validar login: {str(e)}')
+        return False
+
